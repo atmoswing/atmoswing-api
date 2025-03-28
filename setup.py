83d14ea93fc480a5b0cb07759c81034b76ccf0d3
Pascal Horton <pascal.horton@terranum.ch>
2025-03-28 09:15:58 +0100

    Fixing version number and adding badges.

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup
 if __name__ == "__main__":
     setup(
         name="atmoswing-api",
-        version="0.1.0",
+        version="0.9.1",
         author="Pascal Horton",
     )
