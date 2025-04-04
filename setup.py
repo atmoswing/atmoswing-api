0eb1243d32cd4c0022e690a344e7743e2d002284
Pascal Horton <pascal.horton@terranum.ch>
2025-04-04 11:55:46 +0200

    Bumping version number.

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup
 if __name__ == "__main__":
     setup(
         name="atmoswing-api",
-        version="0.9.1",
+        version="0.9.2",
         author="Pascal Horton",
     )
