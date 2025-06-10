f27f8b61069a565f273de994a9c9c46459b38d06
Pascal Horton <pascal.horton@terranum.ch>
2025-06-10 16:27:24 +0200

    Setting the version number in a dedicated file.

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 from setuptools import setup
+from atmoswing_api.__version__ import __version__
 
 if __name__ == "__main__":
     setup(
         name="atmoswing-api",
-        version="1.0.1",
+        version=__version__,
         author="Pascal Horton",
     )
