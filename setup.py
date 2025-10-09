59ec1ffd7300a64b697a6d0acb094ff9b78d7298
Pascal Horton <pascal.horton@terranum.ch>
2025-10-09 22:29:35 +0200

    Fixing project setup definition.

--- setup.py
+++ setup.py
@@ -1,19 +1,6 @@
 #!/usr/bin/env python
-import os
 from setuptools import setup
 
-def read_version():
-    current_dir = os.path.dirname(os.path.realpath(__file__))
-    with open(os.path.join(current_dir, "atmoswing_api", "__version__.py")) as f:
-        for line in f:
-            if line.startswith("__version__"):
-                return line.split("=")[1].strip().strip("'\"")
-        return None
-
-
 if __name__ == "__main__":
-    setup(
-        name="atmoswing-api",
-        version=read_version(),
-        author="Pascal Horton",
-    )
+    # Defer all metadata to pyproject.toml (PEP 621)
+    setup()
